1d362746c62d12e77e96601e9dfb11086b992b42
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-07-23 13:31:12 +0100

    Update code.py

--- Code/code.py
+++ Code/code.py
@@ -9,33 +9,19 @@ droid=gonk()
 droid.display_face(droid.eye)
 droid.reset()
 print(droid.getGyro())
-print("Temperature:",droid.temp)
 
-"""
-droid=gk.gonk()
-droid.display_face(droid.eye)
-print("Recording")
-droid.reset()
-name="movementRightFoot1.csv"
-droid.createFile(name,["x","y","z","s1","s2","s3","s4","s5","s6"])
-
-"""
-#while 1:
-     #droid.blink()
-     #time.sleep(5)
 bot=sinBot()
-demo_geno=np.array([-0.04198164,  0.90918552, -4.65281121, -2.22457433, -0.22812124,  3.07819192,
-                        -0.21745877, -0.47165101,  2.15240473, -1.02080081])
+demo_geno=np.array([0.2540666270398062, 3.3034403579674625, -0.22961708446935794, 3.3551562168512503, -0.5982119772821328, -1.049603120934403, -0.5806661302844076, -0.48036117705384485, -0.5454412746191527, -2.237367193631852])
 bot.set_genotype(demo_geno)
 for i in range(1000):
-    motors=bot.get_positions(0)*50
+    motors=bot.get_positions(0)
     time.sleep(0.1)
     if i%300==0: droid.blink()
-    print(motors)
-    droid.move(0,motors[0])
-    droid.move(1,motors[1])
-    droid.move(2,motors[2])
-    droid.move(3,motors[3])
+    print(droid.sim)
+    droid.move_simulated_angle(0,motors[0])
+    droid.move_simulated_angle(1,motors[1])
+    droid.move_simulated_angle(2,motors[2])
+    droid.move_simulated_angle(3,motors[3])
     time.sleep(0.5)
 
 for i in range(1000):
